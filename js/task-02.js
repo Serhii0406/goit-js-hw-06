f33df46f20e21d295b4311c28a554217f0418c3a
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-10-05 18:31:32 +0300

    task 2
    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elements = ingredients.map(ingredient => {
+  const liRel = document.createElement('li');
+  liRel.textContent = ingredient
+  liRel.classList.add('item');
+  return liRel;
+})
+
+const ulRef = document.querySelector('#ingredients');
+ulRef.append(...elements);
