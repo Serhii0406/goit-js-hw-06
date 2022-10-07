055fc0fb396d40e03bc983a1dfc879a9f1290310
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-10-07 20:28:39 +0300

    add if
    add if when input delete text

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,11 @@ const inputRef = document.querySelector('#name-input');
 const outputRef = document.querySelector('#name-output');
 
 
-inputRef.addEventListener('input', (event) => {
-    const inputValue = event.target.value;
-    outputRef.textContent = inputValue;
-})
+inputRef.addEventListener('input', changeNameIf)
+function changeNameIf(event) {
+   if (event.currentTarget.value !== "") {
+     outputRef.textContent = event.currentTarget.value;
+   } else {
+     outputRef.textContent = "Anonymous";
+}
+}
