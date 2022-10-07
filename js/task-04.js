8c5e05922dbbc7b5b05ef771651351deee7c1d1c
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-10-07 16:43:02 +0300

    task 4
    task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+const button = {
+    sub: document.querySelector('[data-action="increment"]'),
+    add: document.querySelector('[data-action="decrement"]'),
+};
+const {sub, add} = button;
+let counterValue = 0;
+const increment = () => {
+    counterValue += 1;
+    document.getElementById('value').textContent = counterValue;
+};
+const decrement = () => {
+    counterValue -= 1;
+    document.getElementById('value').textContent = counterValue;
+};
+    sub.addEventListener('click', increment);
+    add.addEventListener('click', decrement);
