1ebf2b3cd8b449527d30c089a250d7895ec7a3e0
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-10-08 19:15:57 +0300

    task 8
    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    const {
+        elements: { email, password }, } = event.currentTarget;
+    
+    event.preventDefault();
+    
+    if (email.value === '' || password.value === '') {
+        alert('Please, fill in all fields!')
+    } else {
+        const formData = {
+            email: email.value,
+            password: password.value,
+    }
+     console.log(formData)
+    }
+    event.currentTarget.reset()
+}
