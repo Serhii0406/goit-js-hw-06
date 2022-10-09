1a09707dec13951209ae4bb15d13d55185633676
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-10-09 18:31:26 +0300

    task 9
    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const btnChange = document.querySelector('.change-color');
+const styleWindow = document.querySelector('body');
+const color = document.querySelector('.color');
+
+btnChange.addEventListener('click', () => getRandomHexColor());
+function getRandomHexColor(event) {
+  const createNewColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0);
+  console.log(createNewColor);
+  color.textContent = createNewColor;
+  styleWindow.style.backgroundColor = createNewColor;  
 }
